405308cc53af12a8a53ccdf576617e79a11bfb13
Saray Mendivelso <zaryz2005@gmail.com>
2025-02-26 01:20:18 -0500

    Implementación de eliminación

--- src/main/java/co/edu/eci/ecoappetite/server/ServerApplication.java
+++ src/main/java/co/edu/eci/ecoappetite/server/ServerApplication.java
@@ -2,12 +2,12 @@ package co.edu.eci.ecoappetite.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "co.edu.eci.ecoappetite.server")  // Asegura que Spring escanea todos los paquetes
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
 }
---
+++ src/main/java/co/edu/eci/ecoappetite/server/exception/EcoappetiteException.java
@@ -0,0 +1,7 @@
+package co.edu.eci.ecoappetite.server.exception;
+
+public class EcoappetiteException extends Exception {
+    public EcoappetiteException(String mensaje) {
+        super(mensaje);
+    }
+}
