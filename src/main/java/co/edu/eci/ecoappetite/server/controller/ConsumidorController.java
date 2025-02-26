13bfdcf12d990bf6f72d96df7faca9c0b0fd383d
Jefferson Mesa <mesajefer2@gmail.com>
2025-02-26 18:47:59 -0500

    add answers to bad request with Class BadRequest have a method static

--- src/main/java/co/edu/eci/ecoappetite/server/controller/ConsumidorController.java
+++ src/main/java/co/edu/eci/ecoappetite/server/controller/ConsumidorController.java
@@ -5,6 +5,7 @@ import co.edu.eci.ecoappetite.server.domain.dto.PlatilloDTO;
 import co.edu.eci.ecoappetite.server.exception.EcoappetiteException;
 import co.edu.eci.ecoappetite.server.service.ConsumidorServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,17 +20,29 @@ public class ConsumidorController {
         consumidorServicio.registrarConsumidor(consumidorDTO);
         return ResponseEntity.status(201).body("Consumidor registrado con éxito");
     }
+
     @GetMapping(value = "/id/{id}")
-    public ResponseEntity<ConsumidorDTO> consultarConsumidorPorId(@PathVariable("id") String id) throws EcoappetiteException{
-        var consumidor = consumidorServicio.consultarConsumidorPorId(id);
-        return ResponseEntity.status(200).body(consumidor);
+    public ResponseEntity<ConsumidorDTO> consultarConsumidorPorId(@PathVariable("id") String id) {
+        try {
+            var consumidor = consumidorServicio.consultarConsumidorPorId(id);
+            return ResponseEntity.status(HttpStatus.OK).body(consumidor);
+        } catch (Exception e) {
+            return (ResponseEntity<ConsumidorDTO>) BadRequest.manejarErrores(e);
+        }
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<String> modificarConsumidor(@PathVariable("id") String id, @RequestBody ConsumidorDTO consumidorDTO) throws EcoappetiteException{
-        consumidorServicio.modificarConsumidor(id, consumidorDTO);
-        return ResponseEntity.status(201).body("El consumidor: " + consumidorDTO.getNombre() + " ha sido modificado");
+    public ResponseEntity<String> modificarConsumidor(@PathVariable("id") String id, @RequestBody ConsumidorDTO consumidorDTO) {
+        try {
+            consumidorServicio.modificarConsumidor(id, consumidorDTO);
+            return ResponseEntity.status(HttpStatus.OK).body("El consumidor: " + consumidorDTO.getNombre() + " ha sido modificado");
+        } catch (Exception e) {
+            return (ResponseEntity<String>) BadRequest.manejarErrores(e);
+        }
     }
+
+
+
 
 }
 
